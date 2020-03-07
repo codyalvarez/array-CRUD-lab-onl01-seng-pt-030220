b5ba1da3966d8c6c3dcecec0b4c9b8142f195ec4
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-07 03:18:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array
 
 add_element_to_start_of_array.push("wow")
 
